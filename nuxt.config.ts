7ff5cc791bf7d2372688ffcaa46938d8b9f608b9
xAksssenov <aks.kirill32@gmail.com>
2024-04-06 22:20:01 +0300

    fix: rename title and use another favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+    app: {
+        head: {
+            title: 'Aksenov Kirill',
+            charset: 'utf-8',
+            viewport: 'width=device-width, initial-scale=1',
+        },
+    },
     components: [
         {
             path: '~/components',
