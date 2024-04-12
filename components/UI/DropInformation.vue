2402653e43a1abe310476116da587bd5b1b3c62b
xAksssenov <aks.kirill32@gmail.com>
2024-04-12 23:03:28 +0300

    fix: styles for drop

--- components/UI/DropInformation.vue
+++ components/UI/DropInformation.vue
@@ -1,13 +1,19 @@
 <template>
     <div v-if="show" class="absolute inset-0 flex justify-center items-center">
-        <div class="drop flex flex-row items-center justify-center lg:w-2/3 lg:mx-20 sm:p-5 p-0 rounded-3xl">
+        <div
+            class="drop flex flex-row items-center justify-center lg:w-2/3 lg:mx-20 sm:p-5 p-0 rounded-3xl"
+        >
             <img
                 class="border-2 border-solid border-black rounded-3xl sm:block hidden w-1/2 h-full"
                 :src="image"
                 alt="Image project"
             />
-            <div class="flex flex-col text-center justify-center xl:p-20 sm:p-5 p-1 xl:w-1/2 sm:w-3/4 w-full xl:h-full">
-                <p class="xl:text-4xl sm:text-2xl text-xl p-5">Детали проекта {{ dropText }}</p>
+            <div
+                class="flex flex-col text-center justify-center xl:p-20 sm:p-5 p-1 xl:w-1/2 sm:w-3/4 w-full xl:h-full"
+            >
+                <p class="xl:text-4xl sm:text-2xl text-xl p-5">
+                    Детали проекта {{ dropText }}
+                </p>
                 <div class="flex flex-row justify-center p-3">
                     <NuxtLink
                         class="xl:mx-4 mx-2 2xl:text-2xl xl:text-xl xl:py-5 py-2 lg:px-12 px-5 rounded-full border-2 border-solid border-red-300 cursor-pointer transition-transform duration-500 ease transform hover:scale-110"
